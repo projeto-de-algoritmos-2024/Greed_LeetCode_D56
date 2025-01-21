6a91a05e1f112b23902612ccafbb90c24691821b
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2025-01-20 23:42:15 -0300

    Versão inicial 517 - Erro

--- 517_Super_Washing_Machines/517.py
+++ 517_Super_Washing_Machines/517.py
@@ -1,7 +1,16 @@
 class Solution(object):
     def findMinMoves(self, machines):
-        """
-        :type machines: List[int]
-        :rtype: int
-        """
         
+        total_clothes = sum(machines)
+        n = len(machines)
+
+        if total_clothes % n != 0:
+            return -1
+
+        target = total_clothes // n
+        moves = 0
+
+        for clothes in machines:
+            moves += abs(clothes - target)
+
+        return moves
