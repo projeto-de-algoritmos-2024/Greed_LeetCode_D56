99945c1e9e24677f72cf5738f9962692f73c8032
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2025-01-20 22:41:14 -0300

    Função de verificação

--- 410_Split_Array_Largest_Sum/410.py
+++ 410_Split_Array_Largest_Sum/410.py
@@ -1,8 +1,15 @@
 class Solution(object):
     def splitArray(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        
+        def can_split(max_sum):
+
+            current_sum = 0
+            subarrays = 1
+            for num in nums:
+                if current_sum + num > max_sum:
+                    subarrays += 1
+                    current_sum = num
+                    if subarrays > k:
+                        return False
+                else:
+                    current_sum += num
+            return True
