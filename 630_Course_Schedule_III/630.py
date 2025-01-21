9109048f635b43d51620a2089a16ecda2a6eb037
Caio <caioberg09@gmail.com>
2025-01-20 23:36:41 -0300

    removendo curso
    Removendo curso de maior duração

--- 630_Course_Schedule_III/630.py
+++ 630_Course_Schedule_III/630.py
@@ -1,23 +1,22 @@
+import heapq
+
 class Solution:
     def scheduleCourse(self, courses):
 
-        self.ans = 0
+        # 1) Sort by lastDay
+        courses.sort(key=lambda x: x[1])
 
-        def backtrack(i, current_time, count):
+        total_time = 0
+        max_heap = [] 
 
-            # Atualiza resposta global
-            self.ans = max(self.ans, count)
+        for duration, lastDay in courses:
+            # Adiciona a duração do curso no heap
+            heapq.heappush(max_heap, -duration)
+            total_time += duration
 
-            if i == len(courses):
-                return
+            # se extrapolar o lastDay, remove o curso de maior duração
+            if total_time > lastDay:
+                longest_duration = -heapq.heappop(max_heap)
+                total_time -= longest_duration
 
-            # 1) Não pegar o curso i
-            backtrack(i+1, current_time, count)
-
-            # 2) Tentar pegar o curso i, se couber
-            duration, lastDay = courses[i]
-            if current_time + duration <= lastDay:
-                backtrack(i+1, current_time + duration, count + 1)
-
-        backtrack(0, 0, 0)
-        return self.ans
+        return len(max_heap)
