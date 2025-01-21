41dab975d02c435f5a52f924db4b339423965113
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2025-01-20 22:52:25 -0300

    401 - Primeira versão - Erro

--- 410_Split_Array_Largest_Sum/410.py
+++ 410_Split_Array_Largest_Sum/410.py
@@ -1,15 +1,26 @@
 class Solution(object):
     def splitArray(self, nums, k):
         def can_split(max_sum):
-
             current_sum = 0
             subarrays = 1
             for num in nums:
                 if current_sum + num > max_sum:
                     subarrays += 1
-                    current_sum = num
+                    current_sum = 0 
                     if subarrays > k:
                         return False
                 else:
                     current_sum += num
             return True
+        
+        left = max(nums)
+        right = sum(nums)
+
+        while left <= right: 
+            mid = (left + right) // 2
+            if can_split(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
