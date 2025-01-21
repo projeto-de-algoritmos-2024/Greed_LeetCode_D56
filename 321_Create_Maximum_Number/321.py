f9800b3932115d9d2876d12a48832da46678dcdf
Caio <caioberg09@gmail.com>
2025-01-20 22:54:11 -0300

    trocando if
    tirando if que estava deixando a execução do código longa

--- 321_Create_Maximum_Number/321.py
+++ 321_Create_Maximum_Number/321.py
@@ -9,33 +9,33 @@ class Solution:
                 subseq1 = self.maxSubsequence(nums1, i)
                 # Subsequência de tamanho k-i em nums2
                 subseq2 = self.maxSubsequence(nums2, k - i)
-
+                
                 merged = self.merge(subseq1, subseq2)
                 if merged > best:
                     best = merged
         return best
 
     def maxSubsequence(self, nums, length):
-
         stack = []
         to_remove = len(nums) - length
 
         for num in nums:
 
-            if to_remove > 0 and stack and stack[-1] < num:
+            # ERRO: Usei <=, então se stack[-1] == num, também remove.
+            while stack and to_remove > 0 and stack[-1] <= num:
                 stack.pop()
                 to_remove -= 1
-            
             stack.append(num)
-        
+
+        # Remove se sobrou to_remove
         while to_remove > 0:
             stack.pop()
             to_remove -= 1
-        
+
         return stack[:length]
 
     def merge(self, seq1, seq2):
-
+        # usando greedy (compare seq1[i:] e seq2[j:])
         res = []
         i, j = 0, 0
         while i < len(seq1) and j < len(seq2):
